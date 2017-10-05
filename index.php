aaca507113837ff7d95ba02838494a2b276616b4
Ignatyeff <31989126+Ignatyeff@users.noreply.github.com>
2017-10-05 20:49:45 +0300

    g

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Registration');
 $app->initLayout('Centered');
-/*$button = $app->add(['Button','Кнопка']);
-$button->icon = 'book';
-$button->set(['primary' => true]);*/
+//$button->icon = 'book';
+//$button->set(['primary' => true]);
+$name = 2;
+for ($number = 1; $number <= 9; $number++) {
+  $button = $app->add(['Button',' '.$name]);
+  if (($number %2)==1){
+    $button->set(['primary' => true]);
+  }
+  $name = 2+$name;
+}
